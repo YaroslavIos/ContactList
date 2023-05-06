9d5e179f32fe6ff7b6762d7f96ee814c08d9c4e5
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-05-06 22:10:14 +0300

    Creating model of person.

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,58 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Ярослав Любиченко on 4.5.2023.
+//
+
+import Foundation
+
+struct Person: Identifiable {
+    
+    let id = UUID()
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
+        let phones = DataStore.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
